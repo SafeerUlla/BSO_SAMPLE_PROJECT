a985db78f60465c9c80518565b0a597374ebe4d4
SafeerUlla <safeerulla570@gmail.com>
2024-09-17 15:08:46 +0200

    Initial commit

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+//module.exports = () => ({});
+module.exports = {
+    // Other plugins
+    'io': {
+      enabled: true,
+      config: {
+        contentTypes: [], // Ensure this is an array, even if empty
+        // other configurations
+      },
+    },
+  };
+  
